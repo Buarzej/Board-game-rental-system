976a617a4a50e2037fc83f2ca265c32e06292e11
Błażej Wilkoławski <blazej2801@hotmail.com>
2024-12-25 22:09:08 +0100

    Replaced `async-std` with `tokio`

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
 use futures::executor::block_on;
 use migration::{Migrator, MigratorTrait};
-use sea_orm::{Database, DbErr};
+use sea_orm::{Database, DatabaseConnection, DbErr};
+use tokio::runtime::Runtime;
 
 const DB_NAME: &str = "database";
 const DATABASE_URL: &str = "sqlite:./database.db?mode=rwc";
 
-async fn initialize_database() -> Result<(), DbErr> {
+async fn initialize_database() -> Result<DatabaseConnection, DbErr> {
     let db = Database::connect(DATABASE_URL).await?;
     Migrator::up(&db, None).await?;
 
-    Ok(())
+    Ok(db)
 }
 
 fn main() {
+    let rt = Runtime::new().expect("Failed to create Tokio runtime");
+    
     // Connect to the database and run the migration.
-    if let Err(err) = block_on(initialize_database()) {
-        panic!("{}", err);
-    }
-
-    println!("Connection and migration successful");
+    let db = rt.block_on(initialize_database()).expect("Failed to initialize database");
 }
