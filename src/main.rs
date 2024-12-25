a1ea9efa6254ff5ad7397b528903a35263a1fe76
Błażej Wilkoławski <blazej2801@hotmail.com>
2024-12-25 20:48:22 +0100

    Added a migration process on application start

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
 use futures::executor::block_on;
+use migration::{Migrator, MigratorTrait};
 use sea_orm::{Database, DbErr};
 
-const DATABASE_URL: &str = "sqlite:./database.db?mode=rwc";
 const DB_NAME: &str = "database";
+const DATABASE_URL: &str = "sqlite:./database.db?mode=rwc";
 
-async fn run() -> Result<(), DbErr> {
+async fn initialize_database() -> Result<(), DbErr> {
     let db = Database::connect(DATABASE_URL).await?;
+    Migrator::up(&db, None).await?;
 
     Ok(())
 }
 
 fn main() {
-    if let Err(err) = block_on(run()) {
+    // Connect to the database and run the migration.
+    if let Err(err) = block_on(initialize_database()) {
         panic!("{}", err);
-    } else {
-        println!("Connected to database: {}", DB_NAME);
     }
+
+    println!("Connection and migration successful");
 }
