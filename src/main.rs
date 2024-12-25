d4a229c14369240643c11eade16e6c9559cfc3fb
Błażej Wilkoławski <blazej2801@hotmail.com>
2024-12-25 17:02:20 +0100

    Added basic connection to the SQLite database

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use futures::executor::block_on;
+use sea_orm::{Database, DbErr};
+
+const DATABASE_URL: &str = "sqlite:./database.db?mode=rwc";
+const DB_NAME: &str = "database";
+
+async fn run() -> Result<(), DbErr> {
+    let db = Database::connect(DATABASE_URL).await?;
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = block_on(run()) {
+        panic!("{}", err);
+    } else {
+        println!("Connected to database: {}", DB_NAME);
+    }
 }
