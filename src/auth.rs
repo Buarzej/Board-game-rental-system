5e7f5d7579a87d0780d81b0638921f2eb6169596
Błażej Wilkoławski <blazej2801@hotmail.com>
2025-01-09 02:02:52 +0100

    Added authorization by the JWT tokens

---
+++ src/auth.rs
@@ -0,0 +1,66 @@
+use argon2::password_hash::rand_core::OsRng;
+use argon2::password_hash::SaltString;
+use argon2::{password_hash, Argon2, PasswordHash, PasswordHasher, PasswordVerifier};
+use chrono::{Duration, Utc};
+use jsonwebtoken::{DecodingKey, EncodingKey, Header, Validation};
+use serde::{Deserialize, Serialize};
+use std::env;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct Claims {
+    pub(crate) sub: i32,
+    pub(crate) is_admin: bool,
+    iat: usize,
+    exp: usize,
+}
+
+/// Generates a JWT token for the given user.
+pub(crate) fn generate_jwt(
+    user_id: i32,
+    is_admin: bool,
+) -> Result<String, jsonwebtoken::errors::Error> {
+    let claims = Claims {
+        sub: user_id,
+        is_admin,
+        iat: Utc::now().timestamp() as usize,
+        exp: (Utc::now() + Duration::minutes(30)).timestamp() as usize,
+    };
+    let key = env::var("JWT_SECRET").unwrap();
+
+    jsonwebtoken::encode(
+        &Header::default(),
+        &claims,
+        &EncodingKey::from_secret(key.as_ref()),
+    )
+}
+
+/// Verifies a JWT token and returns the claims if the token is valid.
+pub(crate) fn verify_jwt(token: &str) -> Result<Claims, jsonwebtoken::errors::Error> {
+    let key = env::var("JWT_SECRET").unwrap();
+    jsonwebtoken::decode::<Claims>(
+        token,
+        &DecodingKey::from_secret(key.as_ref()),
+        &Validation::default(),
+    )
+    .map(|data| data.claims)
+}
+
+/// Hashes a password using the Argon2 algorithm.
+pub(crate) fn hash_password(password: String) -> Result<String, password_hash::Error> {
+    let salt = SaltString::generate(&mut OsRng);
+    let argon2 = Argon2::default();
+    argon2
+        .hash_password(password.as_bytes(), &salt)
+        .map(|hash| hash.to_string())
+}
+
+/// Verifies a password against a password hash.
+pub(crate) fn verify_password(
+    password: String,
+    password_hash: String,
+) -> Result<bool, password_hash::Error> {
+    let parsed_hash = PasswordHash::new(&password_hash)?;
+    Ok(Argon2::default()
+        .verify_password(password.as_bytes(), &parsed_hash)
+        .is_ok())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-mod auth_manager;
+mod auth;
 mod db_manager;
 
-use crate::auth_manager::AuthManager;
+use crate::auth::{generate_jwt, hash_password, verify_jwt, verify_password, Claims};
 use crate::db_manager::DatabaseManager;
 use actix_files::NamedFile;
-use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use argon2::password_hash::rand_core::OsRng;
-use argon2::password_hash::SaltString;
-use argon2::{Argon2, PasswordHash, PasswordHasher, PasswordVerifier};
+use actix_web::dev::Payload;
+use actix_web::http::header;
+use actix_web::{get, post, web, App, FromRequest, HttpRequest, HttpResponse, HttpServer};
 use dotenv::dotenv;
 use entity::user::ActiveModel as UserActiveModel;
+use futures::future::{ready, Ready};
 use sea_orm::ActiveValue::Set;
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
@@ -17,7 +17,31 @@ use std::path::PathBuf;
 #[derive(Debug, Clone)]
 struct AppState {
     db: DatabaseManager,
-    auth: AuthManager,
+}
+
+struct Auth(Claims);
+
+impl FromRequest for Auth {
+    type Error = actix_web::Error;
+    type Future = Ready<Result<Self, Self::Error>>;
+
+    fn from_request(req: &HttpRequest, _payload: &mut Payload) -> Self::Future {
+        let token = req
+            .headers()
+            .get(header::AUTHORIZATION)
+            .and_then(|value| value.to_str().ok())
+            .and_then(|str| str.split(" ").nth(1)); // Token is the second word.
+
+        match token {
+            Some(token) => match verify_jwt(token) {
+                Ok(claims) => ready(Ok(Auth(claims))),
+                Err(_) => ready(Err(actix_web::error::ErrorUnauthorized("Invalid token"))),
+            },
+            None => ready(Err(actix_web::error::ErrorUnauthorized(
+                "No token provided",
+            ))),
+        }
+    }
 }
 
 #[derive(Debug, Deserialize)]
@@ -37,19 +61,23 @@ struct LoginFormData {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    dotenv().ok(); // Load environment variables
+    // Load all the necessary resources.
+    dotenv().ok();
+    if std::env::var("JWT_SECRET").is_err() {
+        panic!("JWT_SECRET is not set");
+    }
     let db = DatabaseManager::new(&std::env::var("DATABASE_URL").expect("DATABASE_URL is not set"))
         .await
         .expect("Failed to initialize database");
-    let auth = AuthManager::new().expect("Failed to initialize auth manager");
 
-    let state = AppState { db, auth };
+    let state = AppState { db };
 
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(state.clone()))
             .service(index_register)
             .service(index_login)
+            .service(secret)
             .service(web::scope("/api").service(register).service(login))
     })
     .bind(("127.0.0.1", 8080))?
@@ -71,22 +99,22 @@ async fn index_register(_req: HttpRequest) -> actix_web::Result<NamedFile> {
     Ok(NamedFile::open(path)?)
 }
 
+#[get("/secret")]
+async fn secret(Auth(user): Auth) -> HttpResponse {
+    HttpResponse::Ok().body(format!("Hello, {}!", user.sub))
+}
+
 #[post("/user/login")]
 async fn login(form: web::Form<LoginFormData>, data: web::Data<AppState>) -> HttpResponse {
     match data.db.get_user(form.id).await {
-        Ok(Some(user)) => {
-            match data
-                .auth
-                .verify_password(form.password.clone(), user.password_hash)
-            {
-                Ok(true) => match data.auth.generate_jwt(user.id, user.is_admin) {
-                    Ok(token) => HttpResponse::Ok().body(token),
-                    Err(_) => HttpResponse::InternalServerError().body("Failed to generate JWT"),
-                },
-                Ok(false) => HttpResponse::Unauthorized().body("Invalid password"),
-                Err(_) => HttpResponse::InternalServerError().body("Failed to verify password"),
-            }
-        }
+        Ok(Some(user)) => match verify_password(form.password.clone(), user.password_hash) {
+            Ok(true) => match generate_jwt(user.id, user.is_admin) {
+                Ok(token) => HttpResponse::Ok().body(token),
+                Err(_) => HttpResponse::InternalServerError().body("Failed to generate JWT"),
+            },
+            Ok(false) => HttpResponse::Unauthorized().body("Invalid password"),
+            Err(_) => HttpResponse::InternalServerError().body("Failed to verify password"),
+        },
         Ok(None) => HttpResponse::Unauthorized().body("User not found"),
         Err(_) => HttpResponse::InternalServerError().body("Failed to get user data from database"),
     }
@@ -94,7 +122,7 @@ async fn login(form: web::Form<LoginFormData>, data: web::Data<AppState>) -> Htt
 
 #[post("/user/register")]
 async fn register(form: web::Form<RegisterFormData>, data: web::Data<AppState>) -> HttpResponse {
-    let password_hash = match data.auth.hash_password(form.password.clone()) {
+    let password_hash = match hash_password(form.password.clone()) {
         Ok(hash) => hash,
         Err(_) => return HttpResponse::InternalServerError().body("Failed to hash password"),
     };
